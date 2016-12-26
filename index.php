60a8c31ccf6a158762b9403497b5aaa1fd1d46bc
champiiy <albert1047@hotmail.com>
2016-12-26 11:56:33 -0600

    Agregando

--- index.php
+++ index.php
@@ -6,14 +6,29 @@
 <body id="body1">
 
 		<?php include 'vistas/menu.php';?>
-		<h1 class="titulo">AGENCIA DE PUBLICIDAD</h1>
 
-	<figure class="logo">
+		<h1 class="titulo" style="display:none;">AGENCIA DE PUBLICIDAD</h1>
+
+	<figure class="logo" style="display:none;">
     </figure>
 
 
-    <article class="slogan"><p>Con buena publicidad, vendes mas!!</p></article>
-    <article class="slogan2"><a href="">Contáctanos</a></article>
+    <article class="slogan" style="display:none;"><p>Con buena publicidad, vendes mas!!</p></article>
+    <article class="slogan2" style="display:none;"><a href="">Contáctanos</a></article>
+
+    <script src="jquery.js"></script>
+
+<script type="text/javascript">
+$(document).ready(function(){
+	 $(document).ready(function () {
+		 $('.titulo').fadeIn(2500);
+		 $('.header').fadeIn(2500);
+		 $('.logo').fadeIn(2500);
+		 $('.slogan').fadeIn(2500);
+		 $('.slogan2').fadeIn(2500);
+	 });
+});
+</script>
 
 </body>
 </html>
