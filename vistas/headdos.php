bcbc1ec15117ebba8427269faf1e2b73a724081c
esteban1234 <tva_libra@hotmail.com>
2016-12-28 09:41:42 -0600

    implantacion del ico

--- vistas/headdos.php
+++ vistas/headdos.php
@@ -1,12 +1,18 @@
 <html lang="en">
     <head>
         <meta charset="utf-8">
+        <link rel="icon" type="image/png" href="../img/favicon.png" />
             <title>
                 GRIZZLA ALTA PUBLICIDAD
             </title>
-            <link href="../css/estilosdos.css" rel="stylesheet">
-                <link href="https://fonts.googleapis.com/css?family=Open+Sans|Yanone+Kaffeesatz" rel="stylesheet">
-                    <link href="https://file.myfontastic.com/oZrD95afmh6F7UiEtyxVF9/icons.css" rel="stylesheet">
+            <!-- <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"> -->
+           <META NAME="Title" content="Grizzla Agencia de Publicidad, Marketing y Comunicación Web en Internet especializada en el Desarrollo y Creación de Marcas en Mérida | Agencia de Publicidad, Marketing y Comunicación Web en Internet especializada en el Desarrollo y Creación de Marcas en merida yucatan México">
+           <META NAME="Author" CONTENT="[]"/>
+           <META name="Description" content="Somos una compañía ubicada en la ciudad de Mérida, Yucatán, México, dedicados a ofrecer productos para publicidad, Marketing, e-marketing, lonas, perifoneo y toldos."/>
+           <META NAME="Keywords" CONTENT="productos para publicidad merida yucatan, publicidad merida yucatan, publicidad, GRIZZLA merida yucatan, GRIZZLA yucatan, perifoneo merida yucatan, ampliaciones de casa merida yucatan, Marketing merida yucatan, productos de publicidad, e-marketing, mexico merida yucatan, desarrollo de proyectos de publicidad"/>
+           <link href="../css/estilosdos.css" rel="stylesheet">
+           <link href="https://fonts.googleapis.com/css?family=Open+Sans|Yanone+Kaffeesatz" rel="stylesheet">
+           <link href="https://file.myfontastic.com/oZrD95afmh6F7UiEtyxVF9/icons.css" rel="stylesheet">
                     </link>
                 </link>
             </link>
