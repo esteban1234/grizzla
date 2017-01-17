e7f8602819467a7203bb99602d3a86f604ec356a
esteban1234 <tva_libra@hotmail.com>
2017-01-17 09:38:26 -0600

    codigo de verificacion

--- vistas/headdos.php
+++ vistas/headdos.php
@@ -1,13 +1,14 @@
 <html lang="en">
     <head>
         <meta charset="utf-8">
+        <meta name="google-site-verification" content="Co6EmEwuok6U7bC9S4thIzA8AeRS74-Kfgzw1vTrNew" />
         <link rel="icon" type="image/png" href="../img/favicon.png" />
             <title>
                 GRIZZLA ALTA PUBLICIDAD
             </title>
             <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
            <META NAME="Title" content="Grizzla Agencia de Publicidad, Marketing y Comunicación Web en Internet especializada en el Desarrollo y Creación de Marcas en Mérida | Agencia de Publicidad, Marketing y Comunicación Web en Internet especializada en el Desarrollo y Creación de Marcas en merida yucatan México">
-           <META NAME="Author" CONTENT="[]"/>
+           <META NAME="Author" CONTENT="[http://grizzla.com.mx/]"/>
            <META name="Description" content="Somos una compañía ubicada en la ciudad de Mérida, Yucatán, México, dedicados a ofrecer productos para publicidad, Marketing, e-marketing, lonas, perifoneo y toldos."/>
            <META NAME="Keywords" CONTENT="productos para publicidad merida yucatan, publicidad merida yucatan, publicidad, GRIZZLA merida yucatan, GRIZZLA yucatan, perifoneo merida yucatan, ampliaciones de casa merida yucatan, Marketing merida yucatan, productos de publicidad, e-marketing, mexico merida yucatan, desarrollo de proyectos de publicidad"/>
            <link href="../css/estilosdos.css" rel="stylesheet">
